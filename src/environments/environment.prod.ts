e69aceb2a3d7cf04700a1a7e8c1824360885358e
walyson-scarazzati <walyson.scarazzati@gmail.com>
2021-11-01 17:43:47 +0000

    Mudança de endereço de API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com',
+  apiUrl: 'https://algamoney-api-treinamento.herokuapp.com/',
 
-  tokenAllowedDomains: [ new RegExp('algamoney-api.herokuapp.com') ],
+  tokenAllowedDomains: [ new RegExp('algamoney-api-treinamento.herokuapp.com') ],
   tokenDisallowedRoutes: [ new RegExp('\/oauth\/token') ]
 };
