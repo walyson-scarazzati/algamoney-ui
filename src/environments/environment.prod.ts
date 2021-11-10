096de7a7944edd4f0624a95950857bf5ac619084
walyson-scarazzati <walyson.scarazzati@gmail.com>
2021-11-10 09:05:04 +0000

    Retirando a barra do endereço

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api-treinamento.herokuapp.com/',
+  apiUrl: 'https://algamoney-api-treinamento.herokuapp.com',
 
   tokenAllowedDomains: [ new RegExp('algamoney-api-treinamento.herokuapp.com') ],
   tokenDisallowedRoutes: [ new RegExp('\/oauth\/token') ]
